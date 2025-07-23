db4f6598567987191173c0bb77d626409323bf0c
omanaokar <omanaokar@gmail.com>
2025-07-23 18:17:38 +0530

    Orders project setup

---
+++ OrdersCS/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.Orders
+{
+    internal class Order : IOrderCore
+    {
+        public Order(IOrderCore orderCore, long price, uint quantity, bool isBuySide)
+        {
+
+        }
+
+        //Fields
+        private readonly IOrderCore _orderCore;
+    }
+}
---
+++ OrdersCS/OrderCore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.Orders
+{
+    internal class OrderCore : IOrderCore
+    {
+        public OrderCore(long orderId, string username, int securityId)
+        {
+            OrderId = orderId;
+            Username = username;
+            SecurityId = securityId;
+        }
+
+        public long OrderId { get; private set; }
+        public int SecurityId { get; private set; }
+        public string Username { get; private set; }
+    }
+}
