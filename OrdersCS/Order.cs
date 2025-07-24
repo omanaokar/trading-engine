3472cd38b6a03343cb95665a05bade558727fa5a
omanaokar <omanaokar@gmail.com>
2025-07-24 20:45:43 +0530

    Implemented new order

--- OrdersCS/Order.cs
+++ OrdersCS/Order.cs
@@ -8,7 +8,36 @@ namespace TradingEngineServer.Orders
     {
         public Order(IOrderCore orderCore, long price, uint quantity, bool isBuySide)
         {
+            // PROPERTIES
+            Price = price;
+            IsBuySide = isBuySide;
+            InitialQuantity = quantity;
+            CurrentQuantity = quantity;
 
+            // FIELD
+            _orderCore = orderCore;
+        }
+
+        public long Price { get; private set; }
+        public uint  InitialQuantity { get; private set; }
+        public uint CurrentQuantity { get; private set; }
+        public bool IsBuySide { get; private set; } 
+
+        public long OrderId => _orderCore.OrderId;
+        public string Username => _orderCore.Username;
+        public int SecurityId => _orderCore.SecurityId;
+
+        // METHODS
+        public void increaseQuantity(uint quantityDelta)
+        {
+            CurrentQuantity += quantityDelta;
+        }
+
+        public void decreaseQuantity(uint quantityDelta)
+        {
+            if (quantityDelta > CurrentQuantity)
+                throw new InvalidOperationException($"Quantity Delta > Current Quantity for OrderId={OrderId}");
+            CurrentQuantity -= quantityDelta;
         }
 
         //Fields
