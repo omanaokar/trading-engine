5df7563b9fb131b917c26e541adb9317688465b5
omanaokar <omanaokar@gmail.com>
2025-07-24 20:49:27 +0530

    Implemented cancel order

---
+++ OrdersCS/CancelOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.Orders
+{
+    public  class CancelOrder : IOrderCore
+    {
+        public CancelOrder(IOrderCore orderCore)
+        {
+            _orderCore = orderCore;
+        }
+
+        // PROPERTIES
+        public long OrderId => _orderCore.OrderId;
+        public string Username => _orderCore.Username;
+        public int SecurityId => _orderCore.SecurityId; 
+
+        // FIELDS
+        private readonly IOrderCore _orderCore;
+    }
+}
