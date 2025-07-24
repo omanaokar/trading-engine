8d35b1df7654a01f13b167a44a36db2cee329f80
omanaokar <omanaokar@gmail.com>
2025-07-24 21:35:47 +0530

    Implemented order rejection with reasons

---
+++ OrdersCS/RejectCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TradingEngineServer.Orders;
+
+namespace TradingEngineServer.Rejects
+{
+    public sealed class RejectCreator
+    {
+        public static Rejection GenerateOrderCoreRejection(IOrderCore rejectedOrder, RejectionReason rejectionReason)
+        {
+            return new Rejection(rejectedOrder, rejectionReason);
+        }
+    }
+}
---
+++ OrdersCS/RejectionReason.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.Orders
+{
+    public enum RejectionReason
+    {
+        Unknown,
+        OrderNotFound, 
+        InstrumentNotFound, 
+    }
+}
